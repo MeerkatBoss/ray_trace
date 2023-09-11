3fc416b213a8d26a9dee9e97ff0f624bcc9832a3
MeerkatBoss <ivan.solodovnikov@outlook.com>
2023-09-11 11:31:25 +0300

    feat: add object transform representation

---
+++ src/ray_trace/transform.h
@@ -0,0 +1,66 @@
+/**
+ * @file transform.h
+ * @author MeerkatBoss (solodovnikov.ia@phystech.edu)
+ *
+ * @brief Position, scale and rotation of scene object
+ *
+ * @version 0.1
+ * @date 2023-09-11
+ *
+ * @copyright Copyright MeerkatBoss (c) 2023
+ */
+#ifndef __RAY_TRACE_TRANSFORM_H
+#define __RAY_TRACE_TRANSFORM_H
+
+#include "ray_trace/vec.h"
+#include "ray_trace/matrix.h"
+
+class Transform
+{
+public:
+  Transform(const Point&  position = Point(0, 0, 0),
+            const Vec&    scale    = Point(1, 1, 1),
+            const Matrix& rotation = Matrix::One)
+    : m_position(position), m_scale(scale), m_rotation(rotation)
+  {
+  }
+
+  Transform(const Transform& other) = default;
+  Transform& operator=(const Transform& other) = default;
+
+  const Point&  position() const { return m_position; }
+  const Vec&    scale()    const { return m_scale; }
+  const Matrix& rotation() const { return m_rotation; }
+
+
+  Vec right()    const { return rotation() * Vec::UNIT_X; }
+  Vec left()     const { return -right(); }
+  Vec forward()  const { return rotation() * Vec::UNIT_Y; }
+  Vec backward() const { return -forward(); }
+  Vec up()       const { return rotation() * Vec::UNIT_Z; }
+  Vec down()     const { return -up(); }
+
+  void move  (const Vec& translation) { m_position += translation; }
+
+  void scale(const Vec& scale)
+  {
+    m_scale.m_x *= scale.m_x;
+    m_scale.m_y *= scale.m_y;
+    m_scale.m_z *= scale.m_z;
+  }
+
+  void rotate(const Vec& axis, double angle_deg)
+  {
+    m_rotation *= Matrix::fromRotation(axis, angle_deg);
+  }
+
+  void moveTo (const Vec& target) { m_position  = target; }
+  void scaleTo(const Vec& scale)  { m_scale = scale; }
+
+private:
+  Point  m_position;
+  Vec    m_scale;
+  Matrix m_rotation;
+};
+
+#endif /* transform.h */
